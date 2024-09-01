b2d92f374a437c85f8f7729706502f322e946b79
Leon <2381033@unai.edu>
2024-09-01 17:49:39 +0700

    pertemuan PBO_2_Leon

--- TipeDataArray.java
+++ TipeDataArray.java
@@ -4,7 +4,19 @@ public class TipeDataArray {
         int[] arrayInteger = new int[3];
         arrayInteger[0] = 20;
         arrayInteger[1] = 40;
-        arrayInteger[3] = 50;
-        System.out.println(arrayInteger[0+1]);
+        arrayInteger[2] = 50;
+
+        int[] arrayInteger2 = new int[]{20, 40, 50};
+        arrayInteger2[1] = 60;
+        System.out.println(arrayInteger2);
+
+        int[][] arrayInteger3 = new int[][] {{20, 40, 50}, {90, 100, 120}, {70, 30, 190}};
+        System.out.println(arrayInteger3[0][2] + arrayInteger3[1][2]);
+
+        Object[] arrayOfObject = new Object[3];
+        arrayOfObject[0] = 20;
+        arrayOfObject[1] = "Leon Simanjuntak";
+        arrayOfObject[2] = 20L;
+        System.out.println(arrayOfObject[1]);
     }
 }
--- TipeDataString.java
+++ TipeDataString.java
@@ -1,6 +1,15 @@
 public class TipeDataString {
     public static void main(String[] args) {
         //perbedaan primitive dan nonprimitive
+        String firstName = "Leon";
+        String lastName = "Simanjuntak";
+        String fullname = firstName + " " + lastName;
+        System.out.println(firstName + " " + lastName);
+        System.out.println(firstName.concat(lastName));
+        System.out.println(fullname.toLowerCase());
+        System.out.println(fullname.toUpperCase());
 
+        System.out.println(fullname.charAt(6));
+        System.out.println(fullname.contains("simanjuntak"));
     }
 }
